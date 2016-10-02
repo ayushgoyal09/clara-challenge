e02077d48ccc4c44f4c365e688182d0e7c1e24da
raviteja07 <raviteja07@users.noreply.github.com>
2016-10-02 01:03:08 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # update packages
 sudo apt-get update
 
@@ -9,10 +7,7 @@ sudo apt-get install git-core
 # Install jdk
 sudo apt-get install openjdk-8-jdk
 
-#install
+#install maven
 sudo apt-get install maven
-
-echo 'JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> /etc/environment
-source /etc/environment
 
 mvn clean install
